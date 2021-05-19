17df0dcc2a6c0819722069db278ba0595e2d5f65
Abhishek Kumar <abhishek.ak.vii@gmail.com>
2021-05-19 19:10:47 +0530

    added json file

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 import os
+from googleapiclient.discovery import build
 
 #api credentials from https://console.cloud.google.com/apis/credentials/
-API_KEY = os.environ.get('YOUTUBE_API_KEY')
+api_key = os.environ.get('YOUTUBE_API_KEY')
+
+
+youtubeService = build('youtube', 'v3',developerKey = api_key)
+
+request = youtubeService.channels().list(
+    part = 'statstics',
+    forUsername = '5hM4vtu-38xi_PdX_anGPw'
+)
+response = request.execute()
 
 #check if the key is valid
-print(API_KEY)
+print(api_key)
+
