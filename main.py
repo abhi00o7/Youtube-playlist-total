c093da8f973099c3fa02bbb37c062c3ec027975a
Abhishek Kumar <abhishek.ak.vii@gmail.com>
2021-10-26 23:02:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ from googleapiclient.discovery import build, HttpError
 
 def main():
     #api credentials from https://console.cloud.google.com/apis/credentials/
-    api_key = os.environ.get('YOUTUBE_API_KEY')
+    # api_key = os.environ.get('YOUTUBE_API_KEY')
+    api_key = 'AIzaSyCF-nMsUswu8NYCEtx1JxNoKYTBqgbKcRw'
 
     youtubeService = build('youtube', 'v3',developerKey = api_key)
 
@@ -22,9 +23,13 @@ def main():
         playlistRequest = youtubeService.playlistItems().list(
             #to be used with playlistItems service request
             part = 'contentDetails', 
+            
+            playlistId='PLDN4rrl48XKpZkf03iYFl-O29szjTrs_O',  # ARCH studio Rhino Tutorials
             #id is for the playlist id unlike the channelId which is clearly for the channel id
-            playlistId = 'PLi01XoE8jYohWFPpC17Z-wWhPOSuh8Er-', #ARCH studio Rhino Tutorials
+            # playlistId='PLdo4fOcmZ0oV2uhlVIItfXxuRXsv-gXD5',  # ARCH studio Rhino Tutorials
+            # playlistId = 'PLi01XoE8jYohWFPpC17Z-wWhPOSuh8Er-', #ARCH studio Rhino Tutorials
             maxResults = 50,
+            # maxResults = 50,
             pageToken = nextPageToken
         )
         
